f0ba657143024f490b63443854c7377fa804a1c0
ChristopherValle <53579599+ChristopherValle@users.noreply.github.com>
2019-08-28 22:06:08 -0600

    .cpp & .h

--- Empleado.cpp
+++ Empleado.cpp
@@ -1,17 +1,87 @@
 /*
  * =====================================================================================
- *
- *       Filename:  Empleado.cpp
- *
- *    Description:  Implementación de la Clase Empleado
- *
- *        Created:  2019-08-22
- *
- *         Author:  Maikol Guzman Alan mikeguzman@gmail.com
- *   Organization:  Universidad Nacional de Costa Rica
- *
+ * Christopher Valle Peraza 
+ * Universidad Nacional de Costa Rica
  * =====================================================================================
  */
+#include "Empleado.h"
+#include <iomanip>
+#include <iostream>
 
 
+double Empleado::aumentoSalario(){
+    if(*anosExperienciaPTR >= 1 && *anosExperienciaPTR < 3){
+        *salarioPTR += (((salarioBasePTR * 2)/100) * *anosExperienciaPTR);
+    }
+    if(*anosExperienciaPTR >= 3){
+        *salarioPTR += (((salarioBasePTR * 5)/100) * *anosExperienciaPTR);
+    }
+}
+void Empleado::revisionAleatoria(){
+    if (*revisionPTR = true){
+        *revisionPTR = false;
+    }
+    else{
+        *revisionPTR = true;
+    }
+}
+Empleado::Empleado(){
+    nombreEmpleado = "null";
+    anosExperiencia = 0;
+    identificacion = rand() % 100 + 1;
+    salarioBase = 0;
+    salario = 0;
+    revision = true;
+    nombreEmpleadoPTR = &nombreEmpleado;
+    anosExperienciaPTR = &anosExperiencia;
+    identificacionPTR = &identificacion;
+    salarioBasePTR = &salarioBase;
+    salarioPTR = &salario;
+    revisionPTR = &revision;
+}
+Empleado::Empleado(std::string nombre, int anos, double salarioB){
+    nombreEmpleado = nombre;
+    anosExperiencia = anos;
+    identificacion = rand() % 100 + 1;
+    salarioBase = salarioB;
+    salario = salarioB;
+    revision = true;
+    nombreEmpleadoPTR = &nombreEmpleado;
+    anosExperienciaPTR = &anosExperiencia;
+    identificacionPTR = &identificacion;
+    salarioBasePTR = &salarioBase;
+    salarioPTR = &salario;
+    revisionPTR = &revision;
+}
+std::string Empleado::reporteEmpleado(){
+    std::string rev;
+    if (*revisionPTR = true){
+        rev = "Si";
+    }
+    else if (*revisionPTR = false){
+        rev = "No";
+    }
+    std::string reporte = "Empleado # [" + *identificacionPTR + "]\n\tNombre [" + *nombreEmpleadoPTR + "]\n\tAños de experiencia [" +
+            *anosExperienciaPTR + "]\n\tSalario Inicial [" + *salarioBasePTR + "]\n\tSalario Acumulado [" + *salarioPTR + 
+            "]\n\tNecesita revision [" + rev + "]\n";
+    return reporte;
+}
+std::string Empleado::getNombre(){
+    return *nombreEmpleadoPTR;
+}
+int Empleado::getAnosExperiencia(){
+    return *anosExperienciaPTR;
+}
+double Empleado::getSalarioBase(){
+    return *salarioBasePTR;
+}
+void Empleado::setNombreEmpleado(std::string nombre){
+    nombreEmpleado = nombre;
+}
+void Empleado::setSalarioBase(int anos){
+    anosExperiencia = anos;
+}
+void Empleado::setAnosExperiencia(double salario){
+    salarioBase = salario;
+}
 
--- Empleado.h
+++ Empleado.h
@@ -17,8 +17,35 @@
 #ifndef EMPLEADOPUNTEROS_EMPLEADO_H
 #define EMPLEADOPUNTEROS_EMPLEADO_H
 
-
+#include <stdio.h> 
+#include <stdlib.h>
+#include <time.h>
 #include <string>
+
+class Empleado {
+protected:
+    std::string* nombreEmpleadoPTR{nullptr}, nombreEmpleado;
+    int* anosExperienciaPTR{nullptr}, anosExperiencia;
+    int* identificacionPTR{nullptr}, identificacion;
+    double* salarioBasePTR{nullptr}, salarioBase;
+    double* salarioPTR{nullptr}, salario;
+    bool* revisionPTR{nullptr}, revision;
+    double aumentoSalario();
+    void revisionAleatoria();
+public:
+    Empleado::Empleado();
+    Empleado(std::string nombre, int anos, double salarioBase);
+    std::string reporteEmpleado();
+    std::string getNombre();
+    int getAnosExperiencia();
+    double getSalarioBase();
+    void setNombreEmpleado();
+    void setSalarioBase();
+    void setAnosExperiencia();
+    
+    
+    
+};
 
 
 #endif //EMPLEADOPUNTEROS_EMPLEADO_H
